2211a0fc38657718b609a73728acfa713f12e1ec
장정안 <wjddks96@naver.com>
2023-10-23 00:42:14 +0900

    feat: HISTORY_LIST 내 PRICE를 Number로 변환

--- week1-2/Store.js
+++ week1-2/Store.js
@@ -2,15 +2,26 @@ const tag = "[Store]";
 
 export default class Store {
   constructor(storage) {
-    console.log(tag, "constructor");
+    console.log(tag, "store");
     if (!storage) throw "no storage";
-    this.storage = storage; //여기서 this.storage는 HISTORY_LIST임.
-    console.log("스토어 내 지출 내역", this.storage.HISTORY_LIST);
+    this.storage = storage;
+    console.log("store.js 내 this.storage", this.storage.HISTORY_LIST);
   }
 
   getCheckedData(type) {
     return this.storage.HISTORY_LIST.filter(
       (item) => item.SPEND_OR_INCOME === type
     ); //TODO 이 메서드를 사용해서 filter처리 하고 싶은데, 어떻게 이용할지 모르겠다. 마음이 급하다.
+  }
+
+  deleteList(keyword) {
+    //x 버튼이 눌리면 삭제 이벤트 발행
+    this.storage.HISTORY_LIST = this.storage.HISTORY_LIST.filter(
+      (history) => history.keyword !== keyword
+    );
+    console.log(
+      " deleteList 메서드 내 this.storage.HISTORY_LIST",
+      this.storage.HISTORY_LIST
+    );
   }
 }
