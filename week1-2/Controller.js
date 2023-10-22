03dcd547c9ecb4b4bcd66bde7432fdf4b365e9f5
장정안 <wjddks96@naver.com>
2023-10-22 19:11:26 +0900

    feat: 컨트롤러 & 스토어 업데이트
    mvc 패턴으로 짜려고 노력은 하고 있는데.... Store와 Controller를 잘 이용하지 못하고 있는 것 같습니다...

--- week1-2/Controller.js
+++ week1-2/Controller.js
@@ -1,7 +1,11 @@
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, { historyListView }) {
+  constructor(store, { historyListView, filterView }) {
     console.log(tag, "[Controller]");
+    this.store = store;
+    this.historyListView = historyListView;
+    this.filterView = filterView;
   }
+  //mvc 패턴으로 짜보려고 노력은 하는데, controller를 제대로 쓰지 못하고 있다....
 }
--- week1-2/Store.js
+++ week1-2/Store.js
@@ -4,5 +4,13 @@ export default class Store {
   constructor(storage) {
     console.log(tag, "constructor");
     if (!storage) throw "no storage";
+    this.storage = storage; //여기서 this.storage는 HISTORY_LIST임.
+    console.log("스토어 내 지출 내역", this.storage.HISTORY_LIST);
+  }
+
+  getCheckedData(type) {
+    return this.storage.HISTORY_LIST.filter(
+      (item) => item.SPEND_OR_INCOME === type
+    ); //TODO 이 메서드를 사용해서 filter처리 하고 싶은데, 어떻게 이용할지 모르겠다. 마음이 급하다.
   }
 }
