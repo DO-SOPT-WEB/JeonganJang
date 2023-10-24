03d0ae6b0e98f819ba24f2b1a3ea41e18ee839f4
장정안 <wjddks96@naver.com>
2023-10-25 00:02:13 +0900

    feat: scrollFunction이 html 로드 이후 바로 실행 되도록 변경

--- week1/scrollBtn.js
+++ week1/scrollBtn.js
@@ -1,25 +1,27 @@
-const scrollTopBtn = document.getElementById("scrollTopBtn");
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollTopBtn = document.getElementById("scrollTopBtn");
 
-function scrollFunction() {
-  let scrollTotal =
-    document.body.scrollTop || document.documentElement.scrollTop;
+  function scrollFunction() {
+    let scrollTotal =
+      document.body.scrollTop || document.documentElement.scrollTop;
 
-  const threshold = 20;
-  const maxScrollValue = 3000;
+    const threshold = 20;
+    const maxScrollValue = 3000;
 
-  if (scrollTotal > threshold) {
-    scrollTopBtn.style.display = "block";
-    let opacity = Math.min(scrollTotal / maxScrollValue, 1);
-    scrollTopBtn.style.opacity = opacity;
-  } else {
-    scrollTopBtn.style.display = "none";
+    if (scrollTotal > threshold) {
+      scrollTopBtn.style.display = "block";
+      let opacity = Math.min(scrollTotal / maxScrollValue, 1);
+      scrollTopBtn.style.opacity = opacity;
+    } else {
+      scrollTopBtn.style.display = "none";
+    }
   }
-}
 
-window.onscroll = function () {
-  scrollFunction();
-};
+  window.onscroll = function () {
+    scrollFunction();
+  };
 
-scrollTopBtn.addEventListener("click", () => {
-  window.scroll({ top: 0 });
+  scrollTopBtn.addEventListener("click", () => {
+    window.scroll({ top: 0 });
+  });
 });
