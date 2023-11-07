c490a59d6381c6d4eae6b0d70640b9f0d5017986
장정안 <wjddks96@naver.com>
2023-11-07 23:33:13 +0900

    feat: index.js & App.js 기본 틀 작성

---
+++ week3/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
