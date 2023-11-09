dace6d29cf10921461d2e76a4579630faac9b0fb
장정안 <wjddks96@naver.com>
2023-11-09 21:26:38 +0900

    feat: Theme 정의

---
+++ week3/src/styles/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    primary: "black",
+    secondary: "blue",
+    third: "pink",
+  },
+  fontSizes: {
+    small: "0.5rem",
+    medium: "1rem",
+    large: "2rem",
+  },
+};
